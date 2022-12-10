7d710137ff38455eddd2c6cc95ec4fad9a17c44c
dayzGoBy <georgegrushevsky@yandex.ru>
2022-12-10 14:33:29 +0300

    added writer class

--- disassembler/src/Disassemler.java
+++ disassembler/src/Disassemler.java
@@ -1,38 +1,80 @@
 import java.io.*;
-import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 public class Disassemler {
-    public void parse(String path) {
-        try (Parser p = new Parser(path);){
+    public void convert(String pathFrom, String pathTo) {
+        try (
+                Parser p = new Parser(pathFrom);
+                Writer w = new Writer(pathTo)
+        ){
             p.parseHeader();
+            p.parseText();
+            p.parseSymTable();
         } catch (IOException ex) {
             System.out.println("Error: input/output exception");
         }
     }
 
     public static class Parser implements AutoCloseable {
-        BufferedReader source;
+        DataInputStream source;
         public Parser(String path) {
             try {
-                this.source = new BufferedReader(
-                        new InputStreamReader(new FileInputStream(path))
-                );
+                this.source = new DataInputStream(new FileInputStream(path));
             } catch (FileNotFoundException ex) {
                 System.out.println("Error: file not found");
             }
         }
 
         public void parseHeader() throws IOException {
-            System.out.println(source.read());
+            for(byte b : new byte[]{0x7f, 0x45, 0x4c, 0x46}) {
+                if (b != source.readByte()) {
+                    throw new IllegalStateException("This file is not elf");
+                }
+            }
         }
 
-        public void parseData() {
+        public void parseText() {
 
-        }  
+        }
+
+        public void parseSymTable() {
+
+        }
 
         @Override
         public void close() throws IOException {
             source.close();
+        }
+    }
+
+    public static class Writer implements AutoCloseable {
+        BufferedWriter writer;
+        public Writer(String path) {
+            try {
+                this.writer = new BufferedWriter(new FileWriter(path));
+            } catch (FileNotFoundException ex) {
+                System.out.println("Error: file not found");
+            } catch (IOException e) {
+                System.out.println("I/O error");
+            }
+        }
+
+        public void writeText() throws IOException {
+            writer.write(".text");
+            writer.newLine();
+            // TODO: ???
+            writer.newLine();
+        }
+
+        public void writeSymTab() throws IOException {
+            writer.write(".text");
+            writer.newLine();
+            // TODO: ???
+        }
+
+        @Override
+        public void close() throws IOException {
+            writer.close();
         }
     }
 }
--- disassembler/src/Main.java
+++ disassembler/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        new Disassemler().parse(args[0]);
+        new Disassemler().convert(args[0], args[1]);
     }
 }
